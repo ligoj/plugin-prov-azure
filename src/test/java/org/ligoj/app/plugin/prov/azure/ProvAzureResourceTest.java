5e70f9ce57879da96a20feb653b48b745de33f8a
frup49852 <frup49852@172.16.129.110>
2017-11-26 14:31:50 +0100

    Support multiple location

--- src/test/java/org/ligoj/app/plugin/prov/azure/ProvAzureResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/prov/azure/ProvAzureResourceTest.java
@@ -15,7 +15,6 @@ import org.ligoj.app.model.Node;
 import org.ligoj.app.model.Project;
 import org.ligoj.app.model.Subscription;
 import org.ligoj.app.plugin.prov.ProvResource;
-import org.ligoj.app.plugin.prov.QuoteStorageVo;
 import org.ligoj.app.plugin.prov.QuoteVo;
 import org.ligoj.app.plugin.prov.model.ProvInstance;
 import org.ligoj.app.plugin.prov.model.ProvInstancePrice;
@@ -23,9 +22,10 @@ import org.ligoj.app.plugin.prov.model.ProvInstancePriceType;
 import org.ligoj.app.plugin.prov.model.ProvQuote;
 import org.ligoj.app.plugin.prov.model.ProvQuoteInstance;
 import org.ligoj.app.plugin.prov.model.ProvQuoteStorage;
+import org.ligoj.app.plugin.prov.model.ProvStorageFrequency;
+import org.ligoj.app.plugin.prov.model.ProvStoragePrice;
 import org.ligoj.app.plugin.prov.model.ProvStorageType;
 import org.ligoj.app.plugin.prov.model.VmOs;
-import org.ligoj.app.plugin.prov.model.ProvStorageFrequency;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
@@ -51,10 +51,8 @@ public class ProvAzureResourceTest extends AbstractAppTest {
 	@Before
 	public void prepareData() throws IOException {
 		persistSystemEntities();
-		persistEntities("csv",
-				new Class[] { Node.class, Project.class, Subscription.class, ProvQuote.class, ProvStorageType.class,
-						ProvInstancePriceType.class, ProvInstance.class, ProvInstancePrice.class,
-						ProvQuoteInstance.class, ProvQuoteStorage.class },
+		persistEntities("csv", new Class[] { Node.class, Project.class, Subscription.class, ProvQuote.class, ProvStorageType.class,
+				ProvInstancePriceType.class, ProvInstance.class, ProvInstancePrice.class, ProvQuoteInstance.class, ProvQuoteStorage.class },
 				StandardCharsets.UTF_8.name());
 		subscription = getSubscription("gStack", ProvAzureResource.SERVICE_KEY);
 	}
@@ -89,28 +87,27 @@ public class ProvAzureResourceTest extends AbstractAppTest {
 		Assert.assertTrue(instance.getConstant());
 
 		// Check storage
-		final List<QuoteStorageVo> storages = vo.getStorages();
+		final List<ProvQuoteStorage> storages = vo.getStorages();
 		Assert.assertEquals(4, storages.size());
-		final QuoteStorageVo quoteStorage = storages.get(0);
+		final ProvQuoteStorage quoteStorage = storages.get(0);
 		Assert.assertNotNull(quoteStorage.getId());
 		Assert.assertEquals("server1-root", quoteStorage.getName());
-		Assert.assertEquals(20, quoteStorage.getSize());
+		Assert.assertEquals(20, quoteStorage.getSize().intValue());
 		Assert.assertNotNull(quoteStorage.getQuoteInstance());
-		final ProvStorageType storage = quoteStorage.getType();
+		final ProvStoragePrice storage = quoteStorage.getStorage();
+		final ProvStorageType type = storage.getType();
 		Assert.assertNotNull(storage.getId());
 		Assert.assertEquals(0, storage.getCostGb(), 0.001);
 		Assert.assertEquals(19.71, storage.getCost(), 0.001);
 		Assert.assertEquals(0, storage.getCostTransaction(), 0.001);
-		Assert.assertEquals("P10", storage.getName());
-		Assert.assertEquals(ProvStorageFrequency.HOT, storage.getFrequency());
-
-		//
+		Assert.assertEquals("P10", type.getName());
+		Assert.assertEquals(ProvStorageFrequency.HOT, type.getFrequency());
 
 		// Not attached storage
 		Assert.assertNull(storages.get(3).getQuoteInstance());
 
 		// Transactional costs
-		Assert.assertEquals(0.00000072, storages.get(3).getType().getCostTransaction(), 0.001);
+		Assert.assertEquals(0.00000072, storages.get(3).getStorage().getCostTransaction(), 0.001);
 	}
 
 	@Test
